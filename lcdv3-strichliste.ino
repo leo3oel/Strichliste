9edc95bef73298466024745278b4cc8e5b219107
Leonhard Schraut <spammailspam@aol.de>
2022-10-27 19:09:29 +0200

    Wartungsmodus added, fixed going backwards

--- lcdv3-strichliste.ino
+++ lcdv3-strichliste.ino
@@ -3,9 +3,9 @@
 //#include "functions.h"
 #include<EEPROM.h>//Für dauerspeicher
 //#include <RocketScream_LowPowerAVRZero.h>
-#define ARRAYSIZE 50 //Anzahl der Personen festlegen
+#define ARRAYSIZE 60 //Anzahl der Personen festlegen
 #define interruptPin 2
-LiquidCrystal lcd(12, 11, 5, 4, 3, 9);
+LiquidCrystal lcd(12, 11, 7, 8, 9, 10);
 
 /*ToDo: 
  *  - möglichkeit zum posinlist neu anlegen einführen
@@ -24,12 +24,13 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 9);
 
 
 //Variablen und Konstanten
-const int switchPin = 6; // Striche hochzählen
-const int nschalter = 7; //Namen hochzählen
-const int resetP = 8; //Striche zurücksetzen
-const int switchBackPin = 10;//Striche runterzählen
-const int nschalterBackPin = 11;//Namen zurückschalten
-const int maintenancePin = 12; //Wartungsmodus
+const int switchPin = 3; // Striche hochzählen
+const int nschalter = 2; //Namen hochzählen
+const int resetP = 6; //Striche zurücksetzen
+const int switchBackPin = 4;//Striche runterzählen
+const int nschalterBackPin = 13;//Namen zurückschalten
+const int maintenancePin = 5; //Wartungsmodus
+
 unsigned long int inactivity=0; //sleeptimer
 short int disteeprom=16;//abstand einzelner Personen im EEPROM
 //const int wakeUpPin = 6;
@@ -45,7 +46,6 @@ int prevnschalterstat = 0;
 //Listenposition
 int posinlist = -1; //Zählt personen durch; -1 ist der bitte auswählen bildschirm
 int posinlistprev=0;
-short int disteeprom=16;//abstand einzelner Personen im EEPROM
 
 //Striche zurücksetzen
 int resetS=0;
@@ -64,8 +64,8 @@ int maintenanceState = 0;
 int prevmaintenanceState = 0;
 
 //Arrays für Striche/Namen
-int striche[ARRAYSIZE] = {6,0,12,0,1,0,0};//Striche
-char stnamen[ARRAYSIZE][12] = {"Leonhard S", "Sophie M", "Rainer S", "Georg S", "Simon S"};//Array mit allen Namen, länge auf 12 begrenzt
+int striche[ARRAYSIZE]; //= {6,0,12,0,1,0,0};//Striche
+char stnamen[ARRAYSIZE][12];// = {"Leonhard S", "Sophie M", "Rainer S", "Georg S", "Simon S"};//Array mit allen Namen, länge auf 12 begrenzt
 int usedpers=0; //Anzahl der belegten Plätze im Array, wird später gezählt
 
 
@@ -92,28 +92,28 @@ void setup() //Setup
   //!!!!!!!!!!!!!
   //Nur um neue Mitglieder anzulegen
   //Später bessere Lösung finden:
-  
+  //Serial.begin(9600);
   //EEPROM schreiben
-  //void  writeEEPROM();
+  //writeEEPROM();
+ 
+  //EEPROM lesen 
+  for(int i = 0; i<ARRAYSIZE; i++)
+  {
+    int ieeprom = i*disteeprom;
+    striche[i] = EEPROM.read(ieeprom); //Striche aus EEPROM lesen
+    for(int j = 0; j<12; j++)
+    {
+      stnamen[i][j] = EEPROM.read(ieeprom+j+4); //Einzelne Character aus EEPROM in die Strings einlesen
+    }
+  }
 
   //countarraylength
   usedpers=0;
   for(int i=0; i<ARRAYSIZE; i++)
     if(stnamen[i][0]!=0)
       usedpers++;
-   
-  
-  //EEPROM lesen !!!GEHT NICHT (kann auch problem beim schreiben sein)
-/*  for(int i = 0; i<=usedpers; i++)
-  {
-    int ieeprom = i*disteeprom;
-    striche[i] = EEPROM.read(ieeprom); //Striche aus EEPROM lesen
-    for(int j = 0; j<=12; j++)
-    {
-      stnamen[i][j] = EEPROM.read(ieeprom+j); //Einzelne Character aus EEPROM in die Strings einlesen
-    }    
-  }*/
-  
+
+  //Serial.print(usedpers);
   lcd.begin(16,2);
   pinMode(switchPin,INPUT);
   pinMode(nschalter,INPUT);
@@ -133,15 +133,15 @@ void setup() //Setup
 
 void loop() {
   //sleep inactivity abfrage nach gefühl
-  if(inactivity>100000)
+  if(inactivity>500000)
   {
     inactivity = 0;
     posinlist=-1;
+    sort();
     //sendToSleep();
     lcd.setCursor(0,1);
     lcd.print("Bitte auswaehlen");
   }
-  
 {  //Pins lesen
   nschalterstat = digitalRead(nschalter);
   switchState  = digitalRead(switchPin);
@@ -172,6 +172,8 @@ void loop() {
 
   if(posinlist>=usedpers)//Ende erreicht
     posinlist=-1;
+  if(posinlist<-1)
+    posinlist=usedpers-1;
   if(posinlist!=posinlistprev)//Neuen posinlist anzeigen
     schreiben(posinlist);
 
@@ -260,14 +262,14 @@ void writeEEPROM()
      * Schreibt die kompletten Arrays auf das EEPROM
      * Später noch das schreiben der SD Karte hier ergänzen
      */ 
+    //Serial.println("Schreiben");
     for(int i=0; i<ARRAYSIZE; i++)
     {
       int ieeprom = i*disteeprom;
       EEPROM.update(ieeprom, striche[i]);//Striche schreiben
       for(int j=0; j<12; j++)
       {
-        EEPROM.update(((ieeprom+1)+j),stnamen[i][j]);
-        Serial.println(stnamen[i][j]);
+          EEPROM.update(((ieeprom+4)+j),stnamen[i][j]);
       }
     }
 }
@@ -325,10 +327,99 @@ void wakeUpAgain()
 void maintenance()
 {
   //Display löschen
+  long int des=0;
+  long int del = 0;
+  int count = 0;
   lcd.clear();
   lcd.print("Wartungsmodus");
   lcd.setCursor(0,1);
-  lcd.print("siehe Serial");
+  lcd.print("Serial 9600");
 
-
+  Serial.begin(9600);
+  Serial.println("----------------------------");
+  Serial.println("!Achtung lange Wartezeiten!");
+  Serial.println("Löschen oder Hinzufügen?");
+  Serial.println("1: Löschen");
+  Serial.println("2: Hinzufügen");
+  while(des==0)
+  {
+    des = Serial.parseInt(SKIP_ALL, '\n');
+    count++;
+    delay(2000);
+    if(count>10)
+      break;
+  }
+  if (des == 1)
+  {
+    for(int i=0; i < usedpers; i++)
+    {
+      Serial.print(i+1);
+      Serial.print(": Namen:");
+      Serial.println(stnamen[i]);
+    }
+    Serial.println("Nummer zum Löschen eingeben, zum Abbrechen Zahl größer oder kleiner eingeben");
+    while (del == 0)
+    {
+      del = Serial.parseInt(SKIP_ALL, '\n');
+      count++;
+      delay(2000);
+      if(count>10)
+        break;
+    }
+    if (del>=1 && del <= usedpers)
+    {
+      for (int i = del-1; i<usedpers; i++)//Array zusammenstauchen
+      {
+        striche[i]=striche[i+1];//Striche jeweils eins nach vorne schreiben
+        for (int j = 0; j < 12; j++)
+          stnamen[i][j] = stnamen[i+1][j];//Namen jeweils eins nach vorne schreiben
+      }
+      usedpers--;
+      Serial.println("Person gelöscht");
+    }
+    else
+    {
+      Serial.println("Falsche Nummer, exit");
+      exit;
+    }
+  }
+  else if (des == 2)
+  {//Neue person hinten anlegen
+    if(usedpers == ARRAYSIZE)
+    {
+      Serial.println("Array voll");
+    }
+    else
+    {
+      Serial.println("Achtung max 12 Zeichen");;
+      Serial.println("Namen eingeben:");
+      String in;
+      while (in==0)
+      {
+        in = Serial.readString();
+        count++;
+        delay(2000);
+        if(count>10)
+          break;
+      }
+      for (int i=0;i<12;i++)
+        if (in[i] != '\n')    
+          stnamen[usedpers][i] = in[i];
+      usedpers++;
+      striche[usedpers-1] =0;
+      Serial.println("Person hinzugefügt");
+    }
+  }
+  else
+  {
+    Serial.print("Falsche Nummer, exit");
+    exit;
+  }
+  
+  sort();
+  posinlist = -1;
+  lcd.clear();
+  lcd.print("Name:");
+  lcd.setCursor(8,0);
+  lcd.print("Striche:");
 }
