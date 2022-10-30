a1741b40ceaec4949f7bfc109808f6ae320193c6
leo3oel <spammailspam@aol.de>
2022-10-30 11:59:13 +0100

    commented out serial

--- sleep.ino
+++ sleep.ino
@@ -13,19 +13,19 @@ void sendToSleep()
   attachInterrupt(0, wakeUpAgain, LOW); //Interrupt 0 is connected to digital pin 2
 	set_sleep_mode(SLEEP_MODE_PWR_DOWN); //Full Sleep
 	lcd.noDisplay();
-	Serial.println("going to sleep");
+	//Serial.println("going to sleep");
 	digitalWrite(lcdled, LOW);
 	delay(1000);
   //actually activate sleep mode
   sleep_cpu();
-	Serial.println("just woke up");
+	//Serial.println("just woke up");
 	digitalWrite(lcdled, HIGH);
 	lcd.display();
 }
 
 void wakeUpAgain()
 {
-	Serial.println("Interrupt fired");
+	//Serial.println("Interrupt fired");
   //stop sleep mode
   sleep_disable();
   //clear the interrupt
